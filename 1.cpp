74bba49e3f6aa4bb2caa298d6c8bb03331e72705
adkt <adkt@outlook.com>
2014-07-18 14:25:40 +1000

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,5 +1,3 @@
-//output hex string as image
-
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -7,24 +5,28 @@
 
 std::string* spaceDelimit(std::string,int);
 int hexToDec(std::string);
-std::string decToPixel(int[]);
+std::string decToBin(int);
 int numOfElements(std::string);
+int numOfDigits(int);
+std::string decToBinWidth(int,int);
 
 int main()
 {
+	//18 3C 7E 7E 18 18 18 18
 	std::string pic = "18 3C 7E 7E 18 18 18 18";
 	int picSize = numOfElements(pic);
 	std::string* strArr = new std::string[picSize];
 	
 
 	strArr=spaceDelimit(pic,picSize);
-	int picArr[picSize];
+	std::string picArr[picSize];
 	//output array converted to base 16, 24,61,127 ...	
 	for(int i=0; i<picSize; i++)
 	{
-		picArr[i]=hexToDec(strArr[i]);
+		picArr[i]=decToBinWidth(hexToDec(strArr[i]),8);
 		std::cout << picArr[i] << std::endl;
 	}
+	//std::cout <<  decToBinWidth(147,8) << std::endl;
 	delete[] strArr;
 }
 
@@ -64,9 +66,50 @@ int hexToDec(std::string hex)
 	{
 		if((j=hex[i]-'0')>9)
 		{
-			j = j%16+10;	
+			j = j%16+9;	
 		}
 		num += j*std::pow(16,(1-i));
 	}
 	return num;
+}
+
+std::string decToBin(int dec)
+{
+	std::string bin;
+	while(dec!=1/2)
+	{
+		if(dec%2==0)
+		{
+			bin += "0";
+		}else
+		{
+			bin +="1";
+		}
+		dec/=2;
+	}
+	return bin;
+}
+
+std::string decToBinWidth(int dec, int width)
+{
+	std::string bin;
+	width--;
+	while(width>=0)
+	{
+		if(dec<std::pow(2,width))
+		{
+			bin +="0";
+		}else
+		{
+			bin +="1";
+			dec -=std::pow(2,width);
+		}
+		width--;
+	}
+	return bin;
+}
+
+int numOfDigits(int num)
+{
+	return std::floor(std::log10(std::abs(num))) + 1;
 }
